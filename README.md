df9acdb1686e4f7d5bcda6269247e5609affc3c2
root <root@lin.localdomain>
2022-05-27 04:55:28 +0800

    create autowalk

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # autowalk
-Configurable tool of bulking build path for AutoJump and Ranger
+Configurable tool of bulking build path for AutoJump and Ranger.
+There are `2` main usage you can choose:
+- 🚀 **[ranger](https://github.com/ranger/ranger)** users: you can bulk build jump remap strings for your file system directories that you had set in autowalk configuration file.
+- 🚀 **[autojump](https://github.com/wting/autojump)** users: you can also bulk increase | decrease | clear | list | clean autojump weights for your file system directories that you had set in autowalk configuration file.
+
+## Requirements
+You need to have the following:
+- 🔥 **Python3.6 +**
+- 🚀 **[autojump](https://github.com/wting/autojump)** (Optional: If you only use Ranger to generate remap, you don't need to install it).
+
+## Installing
+[🚀PyPI => AutoWalk ](https://pypi.org/project/autowalk/): 
+
+- you can use `pip` to install it directly:
+
+```shell
+pip install autowalk
+```
+
+## USAGE
+For convenience, command:  `aw`:
+```shell
+aw -h
+```
---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+with open("autowalk/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='autowalk',
+    version="1.1",
+    author="linusic",
+    author_email='cython_lin@cklin.top',
+    description="Configurable tool of bulking build path for AutoJump and Ranger",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/linusic/autowalk",
+    
+    license='MIT',
+    classifiers=[ 
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+    ],
+    entry_points={ 
+        "console_scripts": [ 
+            "aw=autowalk.main:main" 
+        ]
+    },
+    py_modules=["main"],  
+    python_requires='>=3.6',
+
+    packages=(
+        "autowalk",
+        "autowalk.utils",
+    ),
+
+)
