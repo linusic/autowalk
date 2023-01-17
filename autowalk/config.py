011a777b69be06946a53246ce7b32b9b49455cea
linusic <Cython_lin@cklin.top>
2023-01-18 00:55:31 +0800

    Update config.py

--- autowalk/config.py
+++ autowalk/config.py
@@ -1,30 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import re
 from pathlib import Path
 from collections import namedtuple
+import configparser
 
-from utils.assign import Assign
 
-
-class CONFIG:
+class Config(configparser.ConfigParser):
     # auto generate config file for user at home dir
-    GLOBAL_CONFIG = None
-    CONFIG_NAME = ".autowalk.py"  
+    CONFIG_NAME = ".autowalk.cfg"  
 
     # these are default configs, there will be generated into config file
-    DEFAULT_CONFIG = dict(
-        # choose the recursion depth
-        recursion_depth = [2],
-
-        # Config Your Start Dir(One Or More)
+    AUTOJUMP_SECTION = "AutoJump"
+    AUTOJUMP_CONFIG = dict(
+        recursion_depth = 3,
         recursion_root_list = [
-            '/mnt/d',
-            '/mnt/e',
-            '/mnt/d/lin',
+            'd:/',
+            'e:/',
         ],
         black_list_dirname = [
-            # Some is for WSL2 (Windows Sub Linux)
             "$RECYCLE.BIN",
             "BaiduNetdiskDownload",
             "Config.Msi",
@@ -38,26 +32,15 @@ class CONFIG:
             '__',
             '$',
         ],
+        weight_value_only_for_autojump = 1000,
+        autojump_default_config = str(Path.home() / "AppData/Roaming/autojump/autojump.txt"),
 
-        # This Config Just For Autojump 
-        weight_value_only_for_autojump = [
-            1000
-        ],
-
-        autojump_default_config = [
-            "~/.local/share/autojump/autojump.txt"
-        ],
-
-        # This Config Just For Ranger 
-        # If you use ranger you can config it
+        # for ranger
         prefix_and_suffix_only_for_ranger = [
             "g",
             ","
         ],
-        ranger_remap_output_file = [
-            "~/.rc_remap.conf"
-        ],
-
+        ranger_remap_output_file = "~/.rc_remap.conf",
         default_map_only_for_ranger = [
             'map g/ cd /',
             'map g~ cd ~',  # WSL Home
@@ -72,30 +55,97 @@ class CONFIG:
         ]
     )
 
-    @staticmethod
-    def make_config_dict():
+    def __init__(self, allow_no_value=False, *args, **kwargs) -> None:
+        self._allow_no_value = allow_no_value
+        super().__init__(allow_no_value=allow_no_value, *args, **kwargs)
+
+    def seta(self,seciton, option="", value="", *args, **kwargs):
+        if not self.exist(seciton):
+            self[seciton] = {}
+        if option:
+            self[seciton][option] = value
+
+    def exist(self, section, option=None):
+        try:
+            self.get(section, "")
+        except configparser.NoSectionError:
+            return False
+        except configparser.NoOptionError:
+            if option:
+                return True if self.get(section, option, fallback=None) else False
+            else:
+                return True
+
+    def read(self, filenames, encoding=None, must_exist=True):
+        if must_exist and not Path(filenames).exists():
+            raise FileNotFoundError(f"file not found => {filenames}")
+        super().read(filenames, encoding=encoding)
+
+    def write(self, path, mode="w", space_around_delimiters=True) -> None:
+        with Path(path).open(mode) as f:
+            super().write(f, space_around_delimiters=space_around_delimiters)
+            print(f"Complete to write into: {path}")
+
+    def dump_global_config(self, value):
+        if isinstance(value, int):
+            return str(value)
+        elif isinstance(value, (list, tuple, set) ):
+            return "(\n" + "\n".join(value) + "\n)"
+        else:
+            return value # list will be auto join with "," (ConfigParser)
+
+    def load_global_config(self, value):
+        if value:
+            if value.isdigit():
+                return int(value)
+            else:
+                if "\n" not in value:
+                    return value
+                else:
+                    regex_split = re.compile(r"\n\s*")
+                    raw_list = regex_split.split(value)
+
+                    for sym in "()":
+                        raw_list.remove(sym)
+
+                    final_list = [ *map(self.filter_split_for_load_global_config, raw_list ) ]
+                    return final_list
+        else:
+            return value
+        
+    def filter_split_for_load_global_config(self, value):
+        if value.endswith(",") or value.startswith(","):
+            return value.strip(",")
+        else:
+            return value
+        
+    def make_config_dict(self):
         """
             generate config file for user 
             and load global variable for use
         """
-        regex = re.compile(r'(.*?)=([\s\S]*?\])')
-        config_path = (Path.home() / f'{CONFIG.CONFIG_NAME}')
+        config_path = Path.home() / f'{self.CONFIG_NAME}'
 
         if not config_path.exists():
-            with open(str(config_path), "w") as f:
-                final_config_str = Assign.dict_list(CONFIG.DEFAULT_CONFIG)
-                f.write(final_config_str)
+            for k,v in self.AUTOJUMP_CONFIG.items():
+                v = self.dump_global_config(v)
+                self.seta(self.AUTOJUMP_SECTION, k, v) 
+            self.write( str(config_path) )
 
-        with open(str(config_path), "r") as f:
-            config_str = '\n'.join([line for line in f if not line.strip().startswith("#") or not line.strip()])
-            result = regex.findall(config_str.strip())
-            CONFIG.DEFAULT_CONFIG = {k.strip():v.strip() for k,v in result}
-
+        # all values type is str (cat not update dict to other type because of the inner valid)
+        self.read(config_path)
 
         add_name = namedtuple(
             "GLOBAL_CONFIG",
-            CONFIG.DEFAULT_CONFIG.keys()  # add keys
+            self.AUTOJUMP_CONFIG.keys()  # add keys
         )
+        # self.AUTOJUMP_CONFIG = { k:eval(v) for k,v in self.AUTOJUMP_CONFIG.items() }
+        
+        _global_config = self[self.AUTOJUMP_SECTION].values()
+        _global_config = [*map(self.load_global_config, _global_config)]
 
-        CONFIG.DEFAULT_CONFIG = { k:eval(v) for k,v in CONFIG.DEFAULT_CONFIG.items() }
-        CONFIG.GLOBAL_CONFIG = add_name._make(CONFIG.DEFAULT_CONFIG.values())  # add value
+        GLOBAL_CONFIG = add_name._make(
+            _global_config
+        )  # add value
+
+        return GLOBAL_CONFIG
